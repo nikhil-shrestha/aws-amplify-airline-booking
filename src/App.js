6ec7a418f9e391293d9400bc25ae83386b9a1b10
illusionist <nikhil.shrestha1995@gmail.com>
2020-04-15 19:06:35 +0545

    added authentication setup & graphql api setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { withAuthenticator, Authenticator } from 'aws-amplify-react';
+
+import '@aws-amplify/ui/dist/style.css';
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Authenticator usernameAttributes="email" />
     </div>
   );
 }
 
-export default App;
+const signUpConfig = {
+  header: 'Sign Up',
+  hideAllDefaults: true,
+  defaultCountryCode: '1',
+  signUpFields: [
+    {
+      label: 'First Name',
+      key: 'given_name',
+      required: true,
+      displayOrder: 1,
+      type: 'string',
+    },
+    {
+      label: 'Last Name',
+      key: 'family_name',
+      required: true,
+      displayOrder: 2,
+      type: 'string',
+    },
+    {
+      label: 'Luaggage Preference',
+      key: 'luggage_preference',
+      required: true,
+      displayOrder: 3,
+      type: 'string',
+      custom: true,
+    },
+    {
+      label: 'Meal Preference',
+      key: 'meal_preference',
+      required: true,
+      displayOrder: 4,
+      type: 'string',
+      custom: true,
+    },
+    {
+      label: 'Email',
+      key: 'email',
+      required: true,
+      displayOrder: 5,
+      type: 'string',
+    },
+    {
+      label: 'PhoneNumber',
+      key: 'phone_number',
+      required: true,
+      displayOrder: 6,
+      type: 'string',
+    },
+    {
+      label: 'Password',
+      key: 'password',
+      required: true,
+      displayOrder: 7,
+      type: 'password',
+    },
+  ],
+};
+
+export default withAuthenticator(App, {
+  signUpConfig,
+  usernameAttributes: 'email',
+});
